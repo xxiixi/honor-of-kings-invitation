ee698edd9c3209188200dbabf51b0b85d9b64d18
wang xi <1303780098@qq.com>
2026-02-14 22:29:39 +0800

    update url

--- invitation-edit/config.js
+++ invitation-edit/config.js
@@ -4,9 +4,9 @@
 (function (global) {
     global.INVITATION_CONFIG = {
         // 只读邀请页基础地址（生成后的链接会在此基础上追加 ?rank=...&name=... 等参数）
-        readOnlyBaseUrl: 'https://aicoding.juejin.cn/pens/7606617272433852454'
+        readOnlyBaseUrl: 'https://honor-of-kings-invitation.vercel.app/'
 
-        // 本地测试时可将上一行注释，取消下一行注释，并保证同目录下有 invitation-read-only 页面
+        // 本地测试
         // readOnlyBaseUrl: (function () {
         //     var href = location.href;
         //     return href.replace(/\/invitation-edit(\/index\.html)?(\?.*)?$/i, '/invitation-read-only/index.html');
